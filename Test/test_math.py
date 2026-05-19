082043a392121a28b62281f72cbcd4f0e8230c47
Ritesh Pashikanti <riteshpashikanti321@gmail.com>
2026-05-19 16:31:35 +0530

    added flake8 for code quality

--- Test/test_math.py
+++ Test/test_math.py
@@ -1,9 +1,11 @@
-from src.main import add,sub
+from src.main import add, sub
+
 
 def test_add():
-    assert add(2,3) == 5
-    assert add(1,3) == 4
-    
+    assert add(2, 3) == 5
+    assert add(1, 3) == 4
+
+
 def test_sub():
-    assert sub(5,1) == 4
-    assert sub(9,6) == 3    
+    assert sub(5, 1) == 4
+    assert sub(9, 6) == 3
